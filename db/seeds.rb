9a61cf1e9fe24d364450327cc905719ed603921b
Piotr Damer <piotrd@czuby.net>
2020-03-31 16:51:53 +0100

    Expanding the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,39 @@
 
 puts "Creating classes"
 
-new_class = ClassCode.new(name: "ET22", category: "Locomotive", power: "Electric")
-if new_class.save
-	puts "Class #{new_class.name} created"
-else
-	puts "Class #{new_class.name} skipped"
+new_classes = [
+	{ name: "EM10", propulsion: "Electric", category: "Shunting Locomotive", country: "Poland", operator: "PKP" }
+]
+
+puts "#{new_classes.count} classes created"
+
+directories = []
+
+path = Rails.root.join("static")
+
+puts "Accessing #{path}"
+
+Dir.foreach(path) do |dir|
+	next if dir.include?(".")
+	directories << dir
+end
+
+puts "#{directories.count} directories found"
+
+new_classes.each do |new_class|
+	separator = new_class[:country] == "Poland" ? "-" : " "
+	current_directory = "#{new_class[:name].downcase}-#{new_class[:operator].downcase}"
+	if directories.include?(current_directory)
+		Dir.foreach(File.join(path, current_directory)) do |file|
+			if file.include?(".jpg")
+				new_class[:serial_no] = "#{new_class[:name]}#{separator}#{file.split(".")[0]}"
+				new_unit = Unit.new(new_class)
+				if new_unit.save
+					puts "Unit #{new_unit.serial_no} created"
+				else
+					puts "Unit #{new_unit.serial_no} skipped"
+				end
+			end
+		end
+	end
 end
