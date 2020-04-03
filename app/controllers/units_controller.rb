967da1498016b1e6233084c20288b3305f5447f1
Piotr Damer <piotrd@czuby.net>
2020-04-03 15:17:27 +0100

    Additional display logic in the UnitsController

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -2,6 +2,9 @@ class UnitsController < ApplicationController
 	def index
 		if params[:country].present?
 			@units = Unit.where(country: params[:country].capitalize)
+			if params[:cl].present? && params[:operator].present?
+				@units = @units.where("lower(name) = ? AND lower(operator) = ?", params[:cl], params[:operator])
+			end
 			if @units.count == 0
 				redirect_to units_path	
 			end
