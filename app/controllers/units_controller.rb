d1441b25344c591b374724340a8db2de257e3d14
Piotr Damer <piotrd@czuby.net>
2020-04-03 13:17:07 +0100

    Display logic in the UnitsController and Unit index view

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,5 +1,16 @@
 class UnitsController < ApplicationController
 	def index
-		@units = Unit.all
+		if params[:country].present?
+			@units = Unit.where(country: params[:country].capitalize)
+			if @units.count == 0
+				redirect_to units_path	
+			end
+		else
+			@units = Unit.all
+		end
+	end
+
+	def show
+		@unit = Unit.find(params[:id])
 	end
 end
