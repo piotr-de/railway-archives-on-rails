de2597852803490e324a541e0835c76ea8a49c43
Piotr Damer <piotrd@czuby.net>
2020-04-17 18:33:30 +0100

    Seed file clean-up

--- db/seeds.rb
+++ db/seeds.rb
@@ -164,35 +164,6 @@ puts "Total: #{new_classes.count}"
 
 directories = []
 
-# path = Rails.root.join("static")
-
-# puts "Accessing #{path}"
-
-# Dir.foreach(path) do |dir|
-# 	next unless dir.include?("-")
-# 	directories << dir
-# end
-
-# puts "#{directories.count} directories found"
-
-# new_classes.each do |new_class|
-# 	separator = new_class[:country] == "Poland" ? "-" : " "
-# 	current_directory = "#{new_class[:name].downcase}-#{new_class[:operator].downcase}"
-# 	if directories.include?(current_directory)
-# 		Dir.foreach(File.join(path, current_directory)) do |file|
-# 			if file.include?(".jpg") && file.split("-").length == 1
-# 				new_class[:serial_no] = "#{new_class[:name]}#{separator}#{file.split(".")[0]}"
-# 				new_unit = Unit.new(new_class)
-# 				if new_unit.save
-# 					puts "Unit #{new_unit.serial_no} created"
-# 				else
-# 					puts "Unit #{new_unit.serial_no} skipped"
-# 				end
-# 			end
-# 		end
-# 	end
-# end
-
 path = Rails.root.join("static/source")
 
 puts "Accessing source files in #{path}"
