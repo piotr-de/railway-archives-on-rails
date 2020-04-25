27095d8c705133e204d9cde5872e31ead2519319
Piotr Damer <piotrd@czuby.net>
2020-04-25 19:38:30 +0100

    Tidying up the message logic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
 	def send_form
 		@user = params[:user]
-		@message = params[:message]
 		mail(to: ENV["FORM_MAILTO"], subject: "Message from railway.eu.org")
 	end
 end
