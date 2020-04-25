e99eb05af93873d5a601f7ec1ec449a4b9a2db7a
Piotr Damer <piotrd@czuby.net>
2020-04-25 23:14:25 +0100

    Mailgun API test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,14 @@ class UserMailer < ApplicationMailer
 	def send_form
 		@user = params[:user]
 		@message = params[:message]
-		mail(to: ENV["FORM_MAILTO"], subject: "Message from railway.eu.org")
+		mailgun_client = MailGun::Client.new ENV["MAILGUN_API_KEY"]
+		message_params = {
+			from: @user.email,
+			to: ENV["FORM_MAILTO"],
+			subject: "Message from railway.eu.org",
+			message: params[:message]
+		}
+		mailgun_client.send_message ENV["MAILGUN_DOMAIN"], message_params
+		# mail(to: ENV["FORM_MAILTO"], subject: "Message from railway.eu.org")
 	end
 end
