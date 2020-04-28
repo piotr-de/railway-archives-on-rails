c2f0960af3aacda6a828f9e0f87d79277bba5a2e
Piotr Damer <piotrd@czuby.net>
2020-04-28 18:36:38 +0100

    Remove message from User (undo last migration)

--- db/migrate/20200425163724_add_message_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddMessageToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :message, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_175711) do
+ActiveRecord::Schema.define(version: 2020_04_25_131858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2020_04_01_175711) do
     t.string "category"
     t.string "propulsion"
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "encounters", "units"
